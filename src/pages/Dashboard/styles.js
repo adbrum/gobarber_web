3c48ad8877589191e102821646baa9fcc4ea9f00
Adriano Regis <adbrum@outlook.com>
2020-06-11 20:56:17 +0100

    Listing schedules

--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -3,14 +3,13 @@ import styled from 'styled-components';
 export const Container = styled.div`
   max-width: 600px;
   margin: 50px auto;
-
   display: flex;
   flex-direction: column;
 
   header {
     display: flex;
-    align-self: center;
     align-items: center;
+    align-self: center;
 
     button {
       border: 0;
